8545c83fdce9a7b17e53e100b017c693352aeee7
“Bernardo <BCapra@rccl.com>
2020-04-03 15:52:23 -0300

    Added instrumentation testing

--- app/src/main/java/com/globant/counter/mvvm/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/globant/counter/mvvm/viewmodel/MainActivityViewModel.kt
@@ -1,7 +1,7 @@
 package com.globant.counter.mvvm.viewmodel
 
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.globant.counter.mvvm.model.CountModel
 import com.globant.counter.mvvm.viewmodel.states.CounterData
 import com.globant.counter.mvvm.viewmodel.states.CounterState
